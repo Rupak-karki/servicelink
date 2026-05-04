c367ad4cf6dec2e5eb72dfa6a5571bad6d0d1a3f
Rupak-karki <karkirupak137@gmail.com>
2026-05-04 12:51:52 +0545

    register help message modified

--- accounts/forms.py
+++ accounts/forms.py
@@ -14,7 +14,12 @@ class CustomRegistrationForm(UserCreationForm):
     user_type = forms.ChoiceField(choices=[
         ('customer', 'I need services (Customer)'),
         ('provider', 'I offer services (Provider)')
-    ], widget=forms.RadioSelect, initial='customer')
+    ], widget=forms.RadioSelect, initial='customer'
+    # , help_text="Choose whether you want to explore services or provide them."
+    # error_messages={
+    #      'required': "Please select whether you’re a customer or provider."
+    #     }
+    )
     
     class Meta:
         model = User
@@ -23,10 +28,14 @@ class CustomRegistrationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # Add Bootstrap classes to all fields
+
+        #username field
         self.fields['username'].widget.attrs.update({
             'class': 'form-control',
             'placeholder': 'Choose a username'
         })
+
+        #password fields
         self.fields['password1'].widget.attrs.update({
             'class': 'form-control',
             'placeholder': 'Create a password'
@@ -37,5 +46,10 @@ class CustomRegistrationForm(UserCreationForm):
         })
         
         # Customize help text
-        self.fields['username'].help_text = 'Required. 150 characters or fewer.'
+        self.fields['username'].help_text = 'Please choose a unique username.'
+
+        self.fields['email'].help_text = 'Please enter a valid email address. We will never share your email with anyone else.' 
+
         self.fields['password1'].help_text = 'Your password must contain at least 8 characters.'
+
+        # self.fields['password2'].help_text = "Re-enter the same password for confirmation."
