d39f06b87db300dac576466b14c8696c99ee96af
Rupak-karki <karkirupak137@gmail.com>
2026-05-08 18:36:04 +0545

    provider service template modified

--- services/urls.py
+++ services/urls.py
@@ -10,4 +10,11 @@ urlpatterns = [
     path('booking/<int:pk>/<str:status>/', views.update_booking_status, name='update_booking_status'),  # Added this
     path('booking/<int:pk>/cancel/', views.cancel_booking, name='cancel_booking'),  # Added this
     path('my-services/', views.provider_services, name='provider_services'),
+
+    path('add-service/', views.add_service, name='add_service'),
+    
+
+    path('edit-service/<int:pk>/', views.edit_service, name='edit_service'),
+
+    path('delete-service/<int:pk>/', views.delete_service, name='delete_service'),
 ]
