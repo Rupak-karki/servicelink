d30195f0f4ac4b913d7b466805324bf406a4f6af
Rupak-karki <karkirupak137@gmail.com>
2026-05-14 00:56:16 +0545

    fix: profile picture shown

--- servicelink/urls.py
+++ servicelink/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import render
+from django.conf import settings  # Add this
+from django.conf.urls.static import static  # Add this
+from services.models import Category, Service
+from accounts.models import UserProfile
+from django.contrib.auth.models import User
 
 def home(request):
     return render(request, 'home.html')
@@ -11,3 +16,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('services/', include('services.urls')),  # Added this
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
